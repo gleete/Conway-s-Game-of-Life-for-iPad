2db0ffb4e663e0a5b9261c4015ffede8d0caa8a6
gleete <me@gordoleete.com>
2013-12-03 09:54:11 -0500

    Initial Commit

---
+++ iPadLife/iPadLifeAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  iPadLifeAppDelegate.h
+//  iPadLife
+//
+//  Created by Gordon Leete on 12/3/13.
+//  Copyright (c) 2013 Gordon Leete. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface iPadLifeAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iPadLife/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iPadLife
+//
+//  Created by Gordon Leete on 12/3/13.
+//  Copyright (c) 2013 Gordon Leete. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "iPadLifeAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([iPadLifeAppDelegate class]));
+    }
+}
